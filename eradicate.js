10bca79fe5c87048d786295c5dca4dbfcf509103
DavidBograd <david.bjorn.bograd@gmail.com>
2017-06-13 23:24:45 +0200

    added css and restructed eradicate.js

--- eradicate.js
+++ eradicate.js
@@ -1,4 +1,32 @@
-setTimeout(function removeRelated() {
+setInterval(function(){
   var related = document.querySelector('#related');
-  related.innerHTML = '';
-}, 4000);
+	if ( related == null ) {
+		console.log("not loaded yet");
+		return;
+	}
+  else {
+    console.log("removed related");
+    related.innerHTML = '';
+    return;
+  }
+}, 1000);
+
+// var eradicateRetry = setInterval(function(){
+// 		if ( ! isEnabled() ) {
+// 			return;
+// 		}
+//
+// 		// Don't do anything if the FB UI hasn't loaded yet
+// 		var streamContainer = document.getElementById('stream_pagelet');
+// 		if ( streamContainer == null ) {
+//
+// 			return;
+// 		}
+//
+// 		removeNewsFeed();
+//
+// 		// Add News Feed Eradicator quote/info panel
+// 		if ( ! isAlreadyInjected() ) {
+// 			injectUI( streamContainer );
+// 		}
+// }, 1000);
